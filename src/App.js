12d0a8f99f68a0b1ff530dd34589bceeeb3b12f6
dreamer007r <rab1341v@gmail.com>
2023-01-23 20:22:36 +0800

    using hooks

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 import Btn from './components/Btn';
+import { useState } from 'react';
 
 function App() {
-  const eventHandler = () => console.log('clicked');
+  const [eventHandler, setEventHandler] = useState('hello');
+
+  function handleChange(e) { 
+    setEventHandler(e.target.value); 
+  } 
+
   return (
     <div>
       <Nav className="live" />
-      <Btn ev={eventHandler}/>
+      <input value={eventHandler} onChange={handleChange} />
+      <p>You typed: {eventHandler}</p> 
+      <button onClick={() => setEventHandler('hello')}>
+        click me
+      </button>
     </div>
   );
 }
